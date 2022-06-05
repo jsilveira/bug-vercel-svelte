c913974aa6899e5ed77dc63fdf545031fa462ea7
jsilveira <jsilveira@gmail.com>
2022-06-05 20:02:56 -0300

    Simplify more, revert adapter-vercel  version

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,13 @@
-import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		vite: {
-			css: {
-				preprocessorOptions: {
-				}
-			}
-		},
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: vercel({
+			edge: false,
+		}),
 	},
-
-	preprocess: [preprocess({
-	})]
 };
 
 export default config;

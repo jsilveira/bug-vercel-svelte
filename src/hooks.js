ae00d76a4f1e03ad318a1e70950c35c7126bbaee
jsilveira <jsilveira@gmail.com>
2022-06-05 20:44:13 -0300

    More narrowing down

--- src/hooks.js
+++ src/hooks.js
@@ -1,11 +1,12 @@
 import cookie from 'cookie';
 
 export const handle = async ({ event, resolve }) => {
+		// return resolve(event)
 	try {
 		const cookies = cookie.parse(event.request.headers.get('cookie') || '');
 		event.locals.userid = Math.random();
 
-		const options = {ssr: false};
+		const options = {ssr: true};
 
 		const response = await resolve(event, options);
 
@@ -21,7 +22,7 @@ export const handle = async ({ event, resolve }) => {
 			);
 		}
 
-		console.error("Working normally")
+		console.log(`Working normally status=${response.status}`, event.url)
 		return response;
 	} catch(err) {
 		console.error("Error esquivo", err)

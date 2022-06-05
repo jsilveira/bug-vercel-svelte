3e48f1364f5a5253156c0d5697e0bdb406647167
jsilveira <jsilveira@gmail.com>
2022-06-05 20:47:18 -0300

    Getting to the culprit

--- src/hooks.js
+++ src/hooks.js
@@ -3,25 +3,8 @@ import cookie from 'cookie';
 export const handle = async ({ event, resolve }) => {
 		// return resolve(event)
 	try {
-		const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-		event.locals.userid = Math.random();
-
-		const options = {ssr: true};
-
+		const options = {ssr: false};
 		const response = await resolve(event, options);
-
-		if (!cookies.userid) {
-			// if this is the first time the user has visited this app,
-			// set a cookie so that we recognise them when they return
-			response.headers.set(
-				'set-cookie',
-				cookie.serialize('userid', event.locals.userid, {
-					path: '/',
-					httpOnly: true
-				})
-			);
-		}
-
 		console.log(`Working normally status=${response.status}`, event.url)
 		return response;
 	} catch(err) {

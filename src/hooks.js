6a34c04babc9dd87544929cdc6b5b6da8c22990b
jsilveira <jsilveira@gmail.com>
2022-06-05 20:50:51 -0300

    more minimal

--- src/hooks.js
+++ src/hooks.js
@@ -1,14 +1,3 @@
-import cookie from 'cookie';
-
 export const handle = async ({ event, resolve }) => {
-		// return resolve(event)
-	try {
-		const options = {ssr: false};
-		const response = await resolve(event, options);
-		console.log(`Working normally status=${response.status}`, event.url)
-		return response;
-	} catch(err) {
-		console.error("Error esquivo", err)
-		return resolve(event)
-	}
+		return await resolve(event, {ssr: false});
 };
